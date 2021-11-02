63a496d3a9163fcd16cccf9ca43b85ad562904a8
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-11-02 17:22:07 +0100

    init GenieManager.php with selectAllInfoById() function

---
+++ src/Model/GenieManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model;
+
+use App\Model\AbstractManager;
+
+class GenieManager extends AbstractManager
+{
+    public const TABLE = 'genies';
+
+
+    /**
+     * Get one row from database by ID JOIN with specialties table.
+     *
+     */
+    public function showAllInfoById(int $id): array
+    {
+        //Prepare request
+        $query = "SELECT * FROM genies AS g JOIN specialties AS s ON s.id = g.specialties_id WHERE g.id =:id";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
+    }
+}

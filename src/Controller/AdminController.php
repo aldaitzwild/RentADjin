ff4aff82fe82d813328ec2d18da86cd7e76a788f
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-11-02 22:52:51 +0100

    Init adminGeinie view, updated AdminContoller.php and GenieManager.php accordingly

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,9 +3,23 @@
 namespace App\Controller;
 
 use App\Model\UserManager;
+use App\Model\GenieManager;
+use App\Model\SpecialtyManager;
 
 class AdminController extends AbstractController
 {
+    private UserManager $userManager;
+    private GenieManager $genieManager;
+    private SpecialtyManager $specialtyManager;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->userManager = new UserManager();
+        $this->genieManager = new GenieManager();
+        $this->specialtyManager = new SpecialtyManager();
+    }
+
     /**
      * Display home page
      *
@@ -16,9 +30,24 @@ class AdminController extends AbstractController
      */
     public function index(): string
     {
-        $userManager = new UserManager();
-        $users = $userManager->selectAll();
+        $users = $this->userManager->selectAll();
 
         return $this->twig->render('Admin/admin.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * Display page for for one genie
+     *
+     * @return string
+     */
+    public function showGenie($id): string
+    {
+        $genieInfo = $this->genieManager->selectAllInfoById($id);
+        $specialties = $this->specialtyManager->selectAll();
+
+        return $this->twig->render(
+            'Admin/adminGenie.html.twig',
+            ['genieInfo' => $genieInfo, 'specialties' => $specialties]
+        );
     }
 }
--- src/Model/GenieManager.php
+++ src/Model/GenieManager.php
@@ -8,15 +8,14 @@ class GenieManager extends AbstractManager
 {
     public const TABLE = 'genies';
 
-
     /**
      * Get one row from database by ID JOIN with specialties table.
      *
      */
-    public function selectAllInfoById(int $id): array
+    public function selectAllInfoById(int $id)
     {
         //Prepare request
-        $query = "SELECT * FROM genies AS g JOIN specialties AS s ON s.id = g.specialties_id WHERE g.id =:id";
+        $query = "SELECT * FROM genies AS g JOIN specialties AS s ON s.id = g.specialty_id WHERE g.id =:id";
         $statement = $this->pdo->prepare($query);
         $statement->bindValue(':id', $id, \PDO::PARAM_INT);
         $statement->execute();

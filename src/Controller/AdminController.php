1b3caa94e75043f42e4c4ff08b429246a38ecb81
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-11-03 11:38:34 +0100

    updated showGenie function

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -43,11 +43,9 @@ class AdminController extends AbstractController
     public function showGenie($id): string
     {
         $genieInfo = $this->genieManager->selectAllInfoById($id);
-        $specialties = $this->specialtyManager->selectAll();
-
         return $this->twig->render(
-            'Admin/adminGenie.html.twig',
-            ['genieInfo' => $genieInfo, 'specialties' => $specialties]
+            'Admin/adminShowGenie.html.twig',
+            ['genieInfo' => $genieInfo]
         );
     }
 }

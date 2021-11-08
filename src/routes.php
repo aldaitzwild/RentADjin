59f4f69ef04392fa73c6cf23c397c0aa8d4db9ee
Manuela Quertier <manuelaquertier@gmail.com>
2021-11-08 12:32:19 +0100

    grump3

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-   // '' => ['HomeController', 'index',],
+    '' => ['HomeController', 'index',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
@@ -18,5 +18,5 @@ return [
     'admin' => ['AdminController','index',],
     'admin/genies/add' => ['GenieController', 'add',],
     'admin/genies/genie' => ['AdminController', 'showGenie', ['id']],
-    '' => ['WelcomeController', 'index',],
+    'welcome' => ['WelcomeController', 'index',],
 ];

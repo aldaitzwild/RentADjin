e74257e24897c6697385c61d356f32636ebc9c41
Jonathan PERMAROLE <jonathan.permarole@hotmail.com>
2021-11-17 11:10:20 +0100

    fixed routing issues, handled new errors cases on booking

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -25,22 +25,30 @@ class BookingController extends AbstractController
 
         $booking = array_map('trim', $_POST);
         $booking = array_map('htmlentities', $booking);
+        $booking['genieId'] = $geniePageId;
+
         $errors = $this->testInput($booking);
 
         if (!empty($errors['input'])) {
             $_SESSION['errorsBooking'] = $errors;
-            header('Location: /genie?=' . $geniePageId);
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
             return;
         }
 
-        if (!$this->isAvailable(intval($booking["genieId"]), $booking["checkin"], $booking["checkout"])) {
+        if ($booking['checkin'] > $booking['checkout']) {
+            $_SESSION['errorsBooking']["invalid"] = "La date de fin ne peut être antérieur à la date de début";
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+            return;
+        }
+
+        if (!$this->isAvailable($booking["genieId"], $booking["checkin"], $booking["checkout"])) {
             $_SESSION['errorsBooking']["notAvailable"] = "Créneau indisponible";
             return;
         }
 
         $this->bookingManager->insert($booking);
 
-        header('Location: /genie?=' . $geniePageId);
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
     }
 
     public function isAvailable(int $id, string $checkin, string $checkout): bool
